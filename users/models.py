f8fa6be9c5acd24df3a4d8d82c266e41abea1e1f
Dalilla Vidal <dalillavidal@gmail.com>
2025-03-08 17:17:18 -0300

    ajustes na modelagem do usuário e arquivo .env

--- users/models.py
+++ users/models.py
@@ -1,11 +1,9 @@
 from django.db import models
 
-
 class CustomUser(models.Model):
-    id = models.AutoField(primary_key=True)
     nome = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
-    idade = models.IntegerField()
+    idade = models.PositiveIntegerField()  # Usando PositiveIntegerField para garantir valores não negativos
     telefone = models.CharField(max_length=15)
 
     formacao_academica = models.JSONField(null=True, blank=True)
@@ -18,4 +16,7 @@ class CustomUser(models.Model):
 
     def __str__(self):
         return self.nome
-    
+
+    class Meta:
+        verbose_name = 'Usuário Customizado'  # Definindo o nome do modelo para a interface de admin
+        verbose_name_plural = 'Usuários Customizados'  # Definindo o nome plural para a interface de admin
